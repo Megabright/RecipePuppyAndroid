73b3ed6c6b1a2d3c4991b234b8ac03cd5ab3c2bb
mnu <>
2018-12-15 15:34:14 +0100

    Domain Uses Cases with Observables

--- recipepuppydata/src/main/java/com/mundaco/recipepuppy/data/RecipeRepository.kt
+++ recipepuppydata/src/main/java/com/mundaco/recipepuppy/data/RecipeRepository.kt
@@ -4,8 +4,6 @@ import com.mundaco.recipepuppy.data.database.RecipeDao
 import com.mundaco.recipepuppy.data.model.Recipe
 import com.mundaco.recipepuppy.data.network.RecipeApi
 import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class RecipeRepository {
@@ -21,15 +19,12 @@ class RecipeRepository {
         return Observable.fromCallable { recipeDao.search(query) }
             .concatMap { dbRecipeList ->
                 if (dbRecipeList.isEmpty())
-                    recipeApi.getRecipeResponse(query).concatMap {
-                            apiRecipeResponse ->
+                    recipeApi.getRecipeResponse(query).concatMap { apiRecipeResponse ->
                         recipeDao.insertAll(*apiRecipeResponse.results.toTypedArray())
                         Observable.just(apiRecipeResponse.results)
                     }
                 else
                     Observable.just(dbRecipeList)
             }
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
     }
 }
