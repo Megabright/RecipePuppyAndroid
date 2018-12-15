11e55819e96bb5a02712f3107a4a69b59e8bee2f
mnu <>
2018-12-15 16:11:28 +0100

    Proper Injection

--- recipepuppydata/src/main/java/com/mundaco/recipepuppy/data/RecipeRepository.kt
+++ recipepuppydata/src/main/java/com/mundaco/recipepuppy/data/RecipeRepository.kt
@@ -3,16 +3,26 @@ package com.mundaco.recipepuppy.data
 import com.mundaco.recipepuppy.data.database.RecipeDao
 import com.mundaco.recipepuppy.data.model.Recipe
 import com.mundaco.recipepuppy.data.network.RecipeApi
+import com.mundaco.recipepuppy.data.utils.BASE_URL
 import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Inject
 
 class RecipeRepository {
 
-    @Inject
-    lateinit var recipeApi: RecipeApi
+    private val recipeApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+        .build()
+        .create(RecipeApi::class.java)
 
     @Inject
     lateinit var recipeDao: RecipeDao
+
 
     fun searchRecipes(query: String): Observable<List<Recipe>> {
 
