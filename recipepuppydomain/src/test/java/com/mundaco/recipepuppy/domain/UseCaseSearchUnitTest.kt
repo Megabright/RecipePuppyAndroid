45041043c14c05577c3e3ae460620f1658981a0d
mnu <>
2018-12-16 15:26:37 +0100

    Use Case Unit Testing

--- recipepuppydomain/src/test/java/com/mundaco/recipepuppy/domain/UseCaseSearchUnitTest.kt
+++ recipepuppydomain/src/test/java/com/mundaco/recipepuppy/domain/UseCaseSearchUnitTest.kt
@@ -1,18 +1,73 @@
 package com.mundaco.recipepuppy.domain
 
+import com.mundaco.recipepuppy.data.RecipeRepository
+import com.mundaco.recipepuppy.data.model.Recipe
+import io.reactivex.Observable
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.observers.TestObserver
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see [Testing documentation](http://d.android.com/tools/testing)
- */
+@RunWith(RobolectricTestRunner::class)
 class UseCaseSearchUnitTest {
 
+    // Helpers
+
+    class MockRecipeRepositoryImpl: RecipeRepository {
+
+        // Singleton
+        companion object {
+
+            private var INSTANCE: MockRecipeRepositoryImpl? = null
+
+            fun getInstance(): MockRecipeRepositoryImpl {
+                if(INSTANCE == null) INSTANCE = MockRecipeRepositoryImpl()
+                return INSTANCE!!
+            }
+        }
+
+        override fun searchRecipes(query: String): Observable<List<Recipe>> {
+            return Observable.fromArray(emptyList())
+        }
+
+
+    }
+
+    val recipeRepository = MockRecipeRepositoryImpl.getInstance()
+
+
+    val sut: RecipeSearchUseCase = RecipeSearchInteractor(recipeRepository)
+
+    @Before
+    fun setUp() {
+        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
+        RxJavaPlugins.setComputationSchedulerHandler { Schedulers.trampoline() }
+        RxJavaPlugins.setNewThreadSchedulerHandler { Schedulers.trampoline() }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+    }
 
     @Test
-    fun nothing() {
+    fun search_whenEmptyQuery_ReturnsEmptyList() {
 
+        val result = sut.searchRecipes("")
+
+        result.subscribe(TestObserver<List<Recipe>>()
+            .assertComplete()
+            .assertNoErrors()
+            //.assertValueCount(1)
+            //.assertValue(emptyList())
+        )
+
+
+    }
+
+    @After
+    fun dispose() {
 
     }
 }
